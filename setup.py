424b7513f3e647d6f23daf60c86be22be2dfc2f4
Jagdishtripathy <jagadishtripathy144@gmail.com>
2026-01-12 20:02:52 +0530

    Bump version to 1.4.1 (fix APT metadata)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="zenpad",
-    version="1.4.0",
+    version="1.4.1",
     description="A modern, lightweight text editor with features for coding and general use.",
     author="Zenpad Team",
     packages=find_packages(),
